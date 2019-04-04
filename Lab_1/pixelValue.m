b8c497f62b6ce1feee24de7ec6bca8ee9920770e
Marcel van de Lagemaat <marcelvdla@gmail.com>
2019-04-04 16:39:24 +0200

    rotateImage

--- Lab_1/pixelValue.m
+++ Lab_1/pixelValue.m
@@ -5,12 +5,11 @@ if inImage ( size ( image ) ,x , y )
     switch ( method )
         case 'nearest'
             color = image(floor(x + 0.5), floor(y + 0.5));
-        return;
         case 'linear'
             alfa = x - floor(x);
             beta = y - floor(y);
             color = (1-alfa)*(1-beta)*image(floor(x), floor(y))+(1-alfa)*beta*image(floor(x), floor(y+1.0))+alfa*beta*image(floor(x+1.0), floor(y+1.0))+alfa*(1-beta)*image(floor(x+1.0), floor(y));
     end % end switch
 else
-    % return a constant
+    color = -1;
 end
---
+++ Lab_1/rotateImage.m
@@ -0,0 +1,21 @@
+function rotatedImage = rotateImage(image, angle, method)
+    R = [cos(-angle), -sin(-angle); sin(-angle), cos(-angle)];
+    [x_size, y_size] = size(image);
+    center = transpose(repmat([(x_size / 2);(y_size / 2)], x_size*y_size/2, 2));
+    rotatedImage = zeros(x_size, y_size);
+    
+    [X, Y] = meshgrid(1:x_size, 1:y_size);
+    coordinates = transpose([X(:), Y(:)]);
+    
+    canvas = R * (coordinates - center) + center;
+    
+    for i = 1:length(canvas)
+       x = canvas(1, i);
+       y = canvas(2, i);
+       x1 = coordinates(1, i);
+       y1 = coordinates(2, i);
+       color = pixelValue(image, x, y, method);
+       rotatedImage(x1, y1) = color;
+    end
+        
+end

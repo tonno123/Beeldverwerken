250f8c96fc073ea29a2c088f8d764f6684d65cb3
¨tonno123¨ <toonmeijer@gmail.com>
2019-04-09 09:12:06 +0200

    delete everything

--- Lab_1/inImage.m
+++
@@ -1,7 +0,0 @@
-function tmp = inImage(image_size, x, y)
-    if (y >= 1) && (image_size(1) > x) && (x >= 1) && (image_size(2) > y)
-            tmp = 1;
-    else
-        tmp = 0;
-    end
-end
--- Lab_1/profile.m
+++
@@ -1,6 +0,0 @@
-function line = profile ( image , x0 , y0 , x1 , y1 , n , method )
-% profile of an image along straight line in n points
-x = linspace ( x0 , x1 , n ); y = linspace ( y0 , y1 , n );
-for i = 1: length ( x )
-    line ( i ) = pixelValue ( image , x ( i ) , y ( i ) , method );
-end
